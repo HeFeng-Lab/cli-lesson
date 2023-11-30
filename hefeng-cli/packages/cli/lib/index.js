2f4d1d7a224f5187cee8cfe903dc653d6292c1a9
hefeng6500 <2443992009@qq.com>
2023-11-30 14:32:54 +0800

    feat: 添加 commander 基本 demo

--- hefeng-cli/packages/cli/lib/index.js
+++ hefeng-cli/packages/cli/lib/index.js
@@ -1,3 +1,18 @@
+const commander = require("commander")
+
+const { program } = commander
+const pkg = require("../package.json")
+
 module.exports = function (argv) {
-  console.log(111, argv)
+  program.name(Object.keys(pkg.bin)[0]).usage("<command> [options]").version(pkg.version).option("-d, --debug", "是否开启调试模式", false)
+
+  program
+    .command("init [name]")
+    .description("init project")
+    .option("-f, --force", "是否强制更新", false)
+    .action((name, opt) => {
+      console.log("init", name, opt)
+    })
+
+  program.parse(process.argv)
 }
